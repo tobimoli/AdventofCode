d41d87818c6a21d1b6a56f858de55655caad3f0e
tobimoli <63311501+tobimoli@users.noreply.github.com>
2021-12-22 18:57:39 +0100

    Day13

--- Day13/Day13-2.py
+++ Day13/Day13-2.py
@@ -18,24 +18,34 @@ paper = np.zeros(shape = (y_max+1, x_max+1))
 def fold_up(paper, l):
     new_paper  = paper[:l, :]
     fold_paper = paper[l+1:,:]
-    new_paper += fold_paper[::-1]
+    if paper.shape[0]%2 == 1:
+        new_paper += fold_paper[::-1]
+    else:
+        new_paper[1:, :] += fold_paper[::-1]
     return new_paper
 
 def fold_left(paper, l):
     new_paper = paper[:,:l]
     fold_paper = paper[:,l+1:]
-    new_paper += np.flip(fold_paper, axis = 1)
+    if paper.shape[1]%2 == 1:
+        new_paper += np.flip(fold_paper, axis = 1)
+    else:
+        new_paper[:, 1:] += np.flip(fold_paper, axis = 1)
     return new_paper
 
 for p in points:
     x,y = p.split(',')
     paper[int(y), int(x)] = 1
 
-for fold in [folds[0]]:
+for fold in folds:
     s = fold.split(' ')[2]
+    
     if s[0] == 'x':
         paper = fold_left(paper, int(s[2:]))
     elif s[0] == 'y':
         paper = fold_up(paper, int(s[2:]))
 
-print(np.sum(paper>=1))
+for i in range(8):
+    print((paper[:,i*5:(i+1)*5]>0)*1)
+    print()
+
