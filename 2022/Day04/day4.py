ce10fa7167bca65b72c285abcbc995ffd47e8d11
tobimoli <63311501+tobimoli@users.noreply.github.com>
2022-12-04 11:05:29 +0100

    2022 dag 4

---
+++ 2022/Day04/day4.py
@@ -0,0 +1,28 @@
+# %%
+import os
+import re
+import numpy as np
+os.chdir('/Users/tobiasmolenaar/Documents/GitHub/AdventofCode/2022/Day04/')
+with open("input_day4.txt", "r") as f:
+    data = f.read().split("\n")
+
+# part 1 & 2
+full_overlaps = 0
+partially_overlaps = 0
+for section_pair in data:
+    section1, section2 = section_pair.split(',')
+    s1, e1 = section1.split('-')
+    s2, e2 = section2.split('-')
+    s1, e1, s2, e2 = int(s1), int(e1), int(s2), int(e2)
+    
+    set1 = set(range(s1, e1 + 1))
+    set2 = set(range(s2, e2 + 1))
+    overlap = set1 & set2
+    
+    if len(overlap) > 0:
+        partially_overlaps += 1
+    if len(overlap) == len(set1) or len(overlap) == len(set2):
+        full_overlaps += 1
+
+print(full_overlaps)
+print(partially_overlaps)
