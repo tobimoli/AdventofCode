5b5aba32e893d4b031195578ed7c6ca9ace4f188
tobimoli <63311501+tobimoli@users.noreply.github.com>
2022-12-27 17:03:18 +0100

    2022 dag 2 deel 2

--- 2022/Day22/day22.py
+++ 2022/Day22/day22.py
@@ -14,6 +14,40 @@ with open(f"test_day{DAY}.txt", "r") as f:
 FACING = {"right": 0, "down": 1, "left": 2, "up": 3}
 # FACING_INV = {v: k for k, v in FACING.items()}
 
+def make_cube(data, s):
+    lst = [[]] * 14
+    lst[0] = [(0, c, FACING['down']) for c in range(1 * s, 2 * s)]
+    lst[1] = [(r, 0, FACING['right']) for r in range(3 * s, 4 * s)]
+
+    lst[2] = [(r, s, FACING['right']) for r in range(1 * s)]
+    lst[3] = [(r, 0, FACING['right']) for r in reversed(range(2 * s, 3 * s))]
+
+    lst[4] = [(r, s, FACING['right']) for r in range(1 * s, 2 * s)]
+    lst[5] = [(2 * s, c, FACING['down']) for c in range(1 * s)]
+
+    lst[6] = [(0, c, FACING['down']) for c in range(2 * s, 3 * s)]
+    lst[7] = [(4 * s - 1, c, FACING['up']) for c in range(1 * s)]
+
+    lst[8] = [(r, 3 * s - 1, FACING['left']) for r in range(1 * s)]
+    lst[9] = [(r, 2 * s - 1, FACING['left']) for r in reversed(range(2 * s, 3 * s))]
+
+    lst[10] = [(s - 1, c, FACING['up']) for c in range(2 * s, 3 * s)]
+    lst[11] = [(r, 2 * s - 1, FACING['left']) for r in range(1 * s, 2 * s)]
+
+    lst[12] = [(3 * s - 1, c, FACING['up']) for c in range(1 * s, 2 * s)]
+    lst[13] = [(r, s - 1, FACING['left']) for r in range(3 * s, 4 * s)]
+    
+    dic = {}
+    for i in range(len(lst)):
+        face_new = lst[i][0][2]
+        face_old = (face_new + 2) % 4
+        key = [(r, c, face_old) for r, c, _ in lst[i]]
+        if i % 2 == 0:
+            dic.update(dict(zip(key, lst[i + 1])))
+        else:
+            dic.update(dict(zip(key, lst[i - 1])))
+    return dic
+
 
 def read_data(data):
     data1 = data[:-2]
@@ -52,16 +86,23 @@ def find_start_pos(grid):
 
 def get_value_other_side(grid, position):
     row, col, face = position
-    if face == FACING["right"]:
-        col = np.where(grid[row, :] > 0)[0][0]
-    elif face == FACING["left"]:
-        col = np.where(grid[row, :] > 0)[0][-1]
-    elif face == FACING["down"]:
-        row = np.where(grid[:, col] > 0)[0][0]
-    elif face == FACING["up"]:
-        row = np.where(grid[:, col] > 0)[0][-1]
+    if PART2:
+        d = make_cube(DATA, 50)
+        if (row, col, face) in d:
+            row, col, face = d[(row, col, face)]
+        else:
+            print("Wrong input")
     else:
-        print("Wrong face")
+        if face == FACING["right"]:
+            col = np.where(grid[row, :] > 0)[0][0]
+        elif face == FACING["left"]:
+            col = np.where(grid[row, :] > 0)[0][-1]
+        elif face == FACING["down"]:
+            row = np.where(grid[:, col] > 0)[0][0]
+        elif face == FACING["up"]:
+            row = np.where(grid[:, col] > 0)[0][-1]
+        else:
+            print("Wrong face")
     return grid[row, col], (row, col, face)
 
 
@@ -89,13 +130,13 @@ def try_one_step(grid, position):
         elif grid[new_row, new_col] == 2:
             return False, position
         else:
-            value, new_pos_position = get_value_other_side(grid, new_pos_position)
+            value, new_pos_position = get_value_other_side(grid, position)
             if value == 2:
                 return False, position
             else:
                 return True, new_pos_position
     else:
-        value, new_pos_position = get_value_other_side(grid, new_pos_position)
+        value, new_pos_position = get_value_other_side(grid, position)
         if value == 2:
             return False, position
         else:
@@ -123,7 +164,9 @@ def turn(position, leftright):
 
 def follow_path(grid, path):
     position = find_start_pos(grid)
+    # print(grid, path)
     for item in path:
+        print(item, position)
         if isinstance(item, int):
             position = move_steps(grid, position, item)
         else:
@@ -139,9 +182,10 @@ def main(data):
 
 
 # part 1
+PART2 = False
 print(main(TEST))
 print(main(DATA))
 
 # part 2
-# verander get_value_other_side voor de kubus...
-# %%
+PART2 = True
+print(main(DATA))
