9479dcdc02f8db5c822c80fd42fa1321f97a416e
tobimoli <63311501+tobimoli@users.noreply.github.com>
2023-12-17 14:47:05 +0100

    update dag 17

--- 2023/Day17/day17.py
+++ 2023/Day17/day17.py
@@ -1,4 +1,3 @@
-import numpy as np
 import heapq
 
 f = open("input_day.txt", "r")
@@ -15,19 +14,21 @@ def minimal_heat(least, most):
     seen = set()
     while queue:
         heat,x,y,px,py = heapq.heappop(queue)
-        if (x,y) == end: return heat
-        if (x,y, px,py) in seen: continue
-        seen.add((x,y, px,py))
+        if (x, y) == end: 
+            return heat
+        if (x, y, px, py) in seen: 
+            continue
+        seen.add((x, y, px, py))
         # calculate turns only
-        for dx, dy in {(1,0),(0,1),(-1,0),(0,-1)}-{(px,py),(-px,-py)}:
-            a,b,h = x,y,heat
+        for dx, dy in {(1,0),(0,1),(-1,0),(0,-1)}-{(px, py),(-px, -py)}:
+            a, b, h = x, y, heat
             # enter 4-10 moves in the chosen direction
             for i in range(1, most + 1):
                 a, b = a + dx, b + dy
                 if (a, b) in data:
                     h += data[a,b]
                     if i >= least:
-                        heapq.heappush(queue, (h, a,b, dx,dy))
+                        heapq.heappush(queue, (h, a, b, dx, dy))
 
 print(minimal_heat(1, 3))
 print(minimal_heat(4, 10))
