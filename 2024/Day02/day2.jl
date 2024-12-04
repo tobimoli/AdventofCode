f9b72d4003b8e81329f3f6b3f8ab8dc85b6c72c8
tobimoli <63311501+tobimoli@users.noreply.github.com>
2024-12-04 20:05:06 +0100

    day2

--- 2024/Day02/day2.jl
+++ 2024/Day02/day2.jl
@@ -1,12 +1,9 @@
 file = "/Users/tobiasmolenaar/Documents/GitHub/AdventofCode/2024/Day02/input_day.txt"
 
 using DelimitedFiles
-x = readdlm(file)
 
-# part 1
 max_dist = 3
 min_dist = 1
-n_safe = 0
 
 function is_safe(numbers)
     first_num = numbers[1]
@@ -29,9 +26,31 @@ function is_safe(numbers)
     return true
 end
 
-for safe in eachrow(x)
+# part 1
+n_safe = 0
+for safe in eachline(file)
+    safe = [parse(Int64, y) for y in split(safe)]
     if is_safe(safe)
         global n_safe += 1
     end
+end
+print(n_safe)
+
+# part 2
+print('\n')
+n_safe = 0
+for safe in eachline(file)
+    safe = [parse(Int64, y) for y in split(safe)]
+    local bool_safe = false
+    if is_safe(safe)
+        bool_safe = true
+    else
+        for i = 1:length(safe)
+            if is_safe(safe[setdiff(1:end, i)])
+                bool_safe = true
+            end
+        end
+    end
+    global n_safe += bool_safe
 end
 print(n_safe)
